659c0959cc23ed49a2ce15e457608b228d892fb9
Sherrylene Gauci <sherrylene.gauci@zoopla.co.uk>
2020-11-26 22:07:27 +0000

    remove cucumber as it is not required

--- cypress/cucumber.d.ts
+++
@@ -1,8 +0,0 @@
-import { given, when, then, and } from 'cypress-cucumber-preprocessor/steps';
-
-declare global {
-    const Given: typeof given;
-    const When: typeof when;
-    const Then: typeof then;
-    const And: typeof and;
-}
